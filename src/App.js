cc43f777e21aba101128ce804530d7b9e57cf7b6
miguedeabreu <miguedeabreu@gmail.com>
2022-05-10 14:19:53 +0200

    Estilos con styled components

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import Header from "./componentes/header";
 import Inicio from "./componentes/inicio";
 import Blog from "./componentes/blog";
 import AcercaDe from "./componentes/acercaDe";
+import styled from "styled-components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
+      <ContenedorPrincipal>
         <Header />
-        <main>
+        <Main>
           <Routes>
             <Route path="/" element={<Inicio/>} />
             <Route path="/blog" element={<Blog/>} />
             <Route path="/acerca-de" element={<AcercaDe/>} />
           </Routes>
-        </main>
-      </div>
+        </Main>
+      </ContenedorPrincipal>
     </BrowserRouter>
    );
 }
+
+const ContenedorPrincipal = styled.div`
+  padding: 40px;
+  width: 90%;
+  max-width: 700px;
+`;
+
+const Main = styled.main`
+  background: #fff;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
  
 export default App;
